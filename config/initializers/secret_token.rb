e5f6844860ac6b304a1e472f8e304cbe5f467506
Roman Kolesnev <rvkolesnev@gmail.com>
2013-03-06 19:21:18 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AcedRailsTest::Application.config.secret_token = '9a610971bf51ec6671752b20bc5bf30c0801e81b81a16d4ca3ddb5bcd55cf6358374a81316a59e0ee5dcc1921f65617f878c3766c3e5e60cab2e10024191780a'
